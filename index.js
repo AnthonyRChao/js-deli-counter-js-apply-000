4aa0718ce58dc337024c77358b46e7cca4f8ef82
Anthony Chao <anthony.chao@blackrock.com>
2019-03-09 00:36:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  let position = katzDeliLine.length + 1;
+  let position = katzDeliLine.length;
   return "Welcome, " + name + ". You are number " + position + " in line."
 }
 
