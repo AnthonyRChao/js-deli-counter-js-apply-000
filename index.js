bafa90d07d852f797cda8fb704069636d3b1d172
Anthony Chao <anthony.chao@blackrock.com>
2019-03-09 00:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function takeANumber(person) {
   return person
 }
+
+function takeANumber(people, name) {
+  return people, name;
+}
+
+function nowServing(katzDeliLine) {
+  return null 
+}
+
+function currentLine() {
+  return null
+}
