45759cff86b652e170b7b1a282a29ce4242d2c60
Anthony Chao <anthony.chao@blackrock.com>
2019-03-09 00:52:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     // The line is currently: 1. Ada, 2. Grace
     for (var i = 0; i < katzDeliLine.length; i++) {
-      let person = katzDeliLine[i]
-      let position = i + 1
+      let person = katzDeliLine[i];
+      let position = i + 1;
       let str = position + ". " + person +  ", ";
       res += str;
       console.log(res);
