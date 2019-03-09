6c24c1903c29387a718a27b1002271a0bd55edf9
Anthony Chao <anthony.chao@blackrock.com>
2019-03-09 00:38:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return katzDeliLine.pop(0);
+    return "Currently serving" + katzDeliLine.pop(0);
   } else {
     return "There is nobody waiting to be served!";
   }
